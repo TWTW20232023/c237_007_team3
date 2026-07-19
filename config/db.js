43e2171efe359b349350784eeaf2e65617f1c295
hazirah250 <25019778@myrp.edu.sg>
2026-07-19 11:38:18 +0800

    Fix reservation query to match actual database column names, fix signup error handling

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'c237-asyraf-mysql.mysql.database.azure.com',
     user: 'c237_007',
-    password: 'c237_007@2026!',
+    password: 'c237007@2026!',
     database: 'c237_007_team3',
     // Disable SSL certificate verification
     ssl: {rejectUnauthorized: false} 
