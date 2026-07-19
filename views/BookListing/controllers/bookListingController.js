eb07fcd75fe335ef8446dedb002914a9b826f9cd
gohaili <25046283@myrp.edu.sg>
2026-07-19 22:58:46 +0800

    Book list and filter working

---
+++ views/BookListing/controllers/bookListingController.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const Book = require('../models/bookListingModel');
+
+const VIEWS = path.join(__dirname, '../views');
+
+exports.listBooks = (req, res) => {
+  const { title = '', genre = '', status = '' } = req.query;
+
+  Book.getFilteredBooks({ title, genre, status }, (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Database error');
+    }
+
+    Book.getAllGenres((err2, genreResults) => {
+      if (err2) {
+        console.error(err2);
+        return res.status(500).send('Database error');
+      }
+
+      const genres = genreResults.map((row) => row.genre);
+      const statuses = ['available', 'reserved'];
+
+      res.render(path.join(VIEWS, 'booklist'), {
+        books: results,
+        genres,
+        statuses,
+        query: { title, genre, status },
+      });
+    });
+  });
+};
