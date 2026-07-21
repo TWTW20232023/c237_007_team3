09745d9f96e5587daa353e1c0c8b6959ae82bc0c
gohaili <25046283@myrp.edu.sg>
2026-07-21 22:21:13 +0800

    Add all-time popular books

--- views/BookListing/controllers/bookListingController.js
+++ views/BookListing/controllers/bookListingController.js
@@ -7,25 +7,24 @@ exports.listBooks = (req, res) => {
   const { title = '', genre = '', status = '' } = req.query;
 
   Book.getFilteredBooks({ title, genre, status }, (err, results) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Database error');
-    }
+    if (err) { console.error(err); return res.status(500).send('Database error'); }
 
     Book.getAllGenres((err2, genreResults) => {
-      if (err2) {
-        console.error(err2);
-        return res.status(500).send('Database error');
-      }
+      if (err2) { console.error(err2); return res.status(500).send('Database error'); }
 
-      const genres = genreResults.map((row) => row.genre);
-      const statuses = ['available', 'reserved'];
+      Book.getPopularBooks(6, (err3, popularBooks) => {
+        if (err3) { console.error(err3); return res.status(500).send('Database error'); }
 
-      res.render(path.join(VIEWS, 'booklist'), {
-        books: results,
-        genres,
-        statuses,
-        query: { title, genre, status },
+        const genres = genreResults.map((row) => row.genre);
+        const statuses = ['available', 'reserved', 'processing'];
+
+        res.render(path.join(VIEWS, 'booklist'), {
+          books: results,
+          genres,
+          statuses,
+          query: { title, genre, status },
+          popularBooks,
+        });
       });
     });
   });
